4e6e70c5358a2d50662e5e3f748f78f8926da798
bandutes <nojmrk@gmail.com>
2023-03-10 17:01:32 +0200

    Add new driver class

---
+++ src/test/java/lt/noju/pom/utilities/Driver.java
@@ -0,0 +1,44 @@
+package lt.noju.pom.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public void setDriver() {
+
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("start-minimized");
+        options.addArguments("--force-device-scale-factor=0.70");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://demo.seleniumeasy.com/basic-first-form-demo.html");
+
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void setDriver(WebDriver driver) {
+        Driver.driver = driver;
+    }
+
+    public static void closeDriver() {
+        driver.quit();
+
+    }
+
+}
